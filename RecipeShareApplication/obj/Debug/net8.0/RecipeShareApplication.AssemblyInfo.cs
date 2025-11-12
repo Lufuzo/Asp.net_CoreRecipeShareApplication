3a005df45ab0eb9c11acccdea9395fb39122e14e
Lungelo Mbalane <Lungelo Mbalane@LUNGELO-PC>
2025-11-12 14:23:57 +0200

    Updated layout and backgroun dfor the recipe pages

--- RecipeShareApplication/obj/Debug/net8.0/RecipeShareApplication.AssemblyInfo.cs
+++ RecipeShareApplication/obj/Debug/net8.0/RecipeShareApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeShareApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8d633079fde7e2240e325106fdeed4a068ebf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeShareApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeShareApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
